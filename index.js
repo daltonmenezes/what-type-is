e6079f914613ff4bcc4bef33dabb0a4b8140ec2e
Dalton Menezes <daltonmenezes@outlook.com>
2017-10-25 22:25:17 -0200

    Update index.js

--- index.js
+++ index.js
@@ -5,26 +5,4 @@ const { isObject } = require('./src/is/IsObject')
 const { isUndefined } = require('./src/is/isUndefined')
 const { isNull } = require('./src/is/IsNull')
 
-exports.isArray = (it) => {
-  return isArray(it)
-}
-
-exports.isString = (it) => {
-  return isString(it)
-}
-
-exports.isNumber = (it) => {
-  return isNumber(it)
-}
-
-exports.isObject = (it) => {
-  return isObject(it)
-}
-
-exports.isUndefined = (it) => {
-  return isUndefined(it)
-}
-
-exports.isNull = (it) => {
-  return isNull(it)
-}
+module.exports = {isArray, isString, isNumber, isObject, isUndefined, isNull}
