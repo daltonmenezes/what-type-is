5e22a7655578b1e63c4ac7398373c751a7ce220e
Dalton Menezes <daltonmenezes@outlook.com>
2017-10-26 01:35:47 -0200

    Refactor isArray test

--- test/IsArrayTest.js
+++ test/IsArrayTest.js
@@ -6,23 +6,11 @@ describe('isArray', () => {
       expect(isArray([])).to.be.true
   })
 
-  it('should return false when an Object is passed as argument', () => {
-      expect(isArray({})).to.be.false
-  })
-
-  it('should return false when a Number is passed as argument', () => {
-      expect(isArray(1)).to.be.false
-  })
-
-  it('should return false when a String is passed as argument', () => {
-      expect(isArray('')).to.be.false
-  })
-
-  it('should return false when an Undefined is passed as argument', () => {
-      expect(isArray(undefined)).to.be.false
-  })
-
-  it('should return false when a Null is passed as argument', () => {
+  it('should return false when the argument passed is not a Array', () => {
       expect(isArray(null)).to.be.false
+      expect(isArray(1)).to.be.false
+      expect(isArray({})).to.be.false
+      expect(isArray(undefined)).to.be.false
+      expect(isArray('')).to.be.false
   })
 })
